32feef2bc94a57d4b47a6cf554b231e9bf29fc00
Audrius Puplinskas <audrius@ipk.lt>
2024-07-16 22:13:44 +0300

    Add files via upload

---
+++ vsm_srv.h
@@ -0,0 +1,49 @@
+#ifndef _VRE_APP_Sm_
+#define	_VRE_APP_Sm_
+
+#define bool int
+#define true 1
+#define false 0
+#define TOKEN_BAD 'b'
+#define TOKEN_END 'e'
+#define TOKEN_NUMBER 'n'
+
+#include "vmsys.h"
+#include "vmsm.h"
+#include "vmio.h"
+#include "vmgraph.h"
+#include "vmchset.h"
+#include "vmstdlib.h"
+
+#include <stdlib.h>
+#include "stdio.h"
+#include <string.h>
+#include "vmpromng.h"
+#include "stdint.h"
+
+typedef uint8_t u8;
+typedef double f64;
+
+enum {
+    id_get_hello_string = 1,
+    id_get_hello_string2 = 2,
+    id_get_hello_string3 = 3,
+    id_get_hello_string4 = 4
+};
+
+VMINT handle_smevt(VMINT message, VMINT param);
+void get_hello_string(VMSTR str, VMINT len);
+void get_hello_string2(VMSTR str, VMINT len);
+void get_hello_string3(VMSTR str, VMINT len);
+void get_hello_string4(VMSTR str, VMINT len, VMSTR input_data);
+
+bool next_token();
+bool token_is(int kind);
+bool match_token(int kind);
+f64 parse_level_three();
+f64 parse_level_two();
+f64 parse_level_one();
+f64 parse_level_zero();
+f64 parse_string(char * string);
+
+#endif
